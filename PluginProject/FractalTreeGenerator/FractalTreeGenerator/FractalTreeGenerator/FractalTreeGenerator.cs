f9f01bf68d89494bcc40645f164644586ae806bf
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-12-16 23:45:35 +0200

    finishing fractal tree plugin C# project

--- PluginProject/FractalTreeGenerator/FractalTreeGenerator/FractalTreeGenerator/FractalTreeGenerator.cs
+++ PluginProject/FractalTreeGenerator/FractalTreeGenerator/FractalTreeGenerator/FractalTreeGenerator.cs
@@ -3,27 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Events;
 
 namespace FractalTreeGenerator
 {
-    [TransactionAttribute(TransactionMode.Manual)]
-    public class GenerateFractalTree : IExternalCommand
+    //class to store variables as globals 
+    public static class GlobVars
     {
+        //Default tree depth
+        public static int treeDepth = 7;
+
         //Hardcoded tree values
         public static double rotAngle = 34;
         public static double lenFactor = 0.78;
         public static double rndCoof = 0.15;
-        int treeDepth = 12;
 
         //setting a randomizer
         public static Random random = new Random();
 
         //Line list to fill
-        static List<Curve> tree = new List<Curve>();
+        public static List<Curve> tree = new List<Curve>();
+    }
 
+    //class with UI generations, which calls the main treegeneration class
+    public class FractalPanel : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication app)
+        {
+            //Addin tab data
+            string RIBBON_PANEL = "Generate fractal tree";
+
+            //Add a tab
+            RibbonPanel panel = app.CreateRibbonPanel(RIBBON_PANEL);
+
+            string assemblyName = Assembly.GetExecutingAssembly().Location;
+
+            //add button for command trigger
+            PushButtonData buttonData = new PushButtonData(
+                "Generate a fractal tree from a line", RIBBON_PANEL, assemblyName,
+                "FractalTreeGenerator.GenerateFractalTree");
+
+            PushButton pushButton = panel.AddItem(buttonData) as PushButton;
+            pushButton.ToolTip = "Click on a line to turn into a fractal tree";
+
+            //add text input
+            TextBoxData itemDepth = new TextBoxData("treeDepth");
+            TextBox item1 = panel.AddItem(itemDepth) as TextBox;
+            item1.Value = 3;
+            item1.ToolTip = "Tree depth (do not use more than 10)";
+            item1.EnterPressed += Refresh;
+
+            //refreshes value picked from textbox on enter press
+            void Refresh(object sender, TextBoxEnterPressedEventArgs args)
+            {
+                try
+                {
+                    TextBox textBoxRefresher = sender as TextBox;
+                    GlobVars.treeDepth = Convert.ToInt32(item1.Value.ToString());
+                }
+                catch
+                { 
+                }
+            }
+
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // nothing to clean up in this simple case
+            return Result.Succeeded;
+        }
+    }
+
+
+    //main class for tree generation
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class GenerateFractalTree : IExternalCommand
+    {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             //Get UIDocument
@@ -45,22 +106,22 @@ namespace FractalTreeGenerator
                 Element line = doc.GetElement(lineId);
                 Curve crv = (line.Location as LocationCurve).Curve;
 
+                //If an element was picked run tree generation
                 if (refLine != null)
                 {
                     //call recursive function
-                    TreeRecursion(treeDepth, crv);
+                    TreeRecursion(GlobVars.treeDepth, crv);
 
                     //Place generated lines in Revit
                     using (Transaction trans = new Transaction(doc, "Place Tree"))
                     {
                         trans.Start();
 
-                        foreach (Curve c in tree)
+                        foreach (Curve c in GlobVars.tree)
                         {
                             //Wall.Create(doc, c, level.Id, false);
                             doc.Create.NewDetailCurve(view, c);
                         }
-
 
                         trans.Commit();
                     }
@@ -73,28 +134,32 @@ namespace FractalTreeGenerator
                 message = e.Message;
                 return Result.Failed;
             }
-
         }
+
+
+        //Converts degree to radians
         public static double ToRad(double angle)
         {
             return Math.PI * angle / 180.0;
         }
 
+        //generating a random val
         public static int Rnder(double value)
         {
-            //generating a random val
-            int result = random.Next(Convert.ToInt32(value - (value * rndCoof)), Convert.ToInt32(value + (value * rndCoof)));
+            int result = GlobVars.random.Next(Convert.ToInt32(value - (value * GlobVars.rndCoof)),
+                Convert.ToInt32(value + (value * GlobVars.rndCoof)));
 
             return result;
         }
 
+        //Tree recursive method
         public static void TreeRecursion(int depth, Curve branch)
         {
             if (depth > 0)
             {
                 //getting angle values
-                double radPlus = ToRad(Rnder(rotAngle));
-                double radMinus = -ToRad(Rnder(rotAngle));
+                double radPlus = ToRad(Rnder(GlobVars.rotAngle));
+                double radMinus = -ToRad(Rnder(GlobVars.rotAngle));
 
                 //Get start and end points
                 XYZ start = branch.GetEndPoint(0);
@@ -102,14 +167,12 @@ namespace FractalTreeGenerator
 
                 //Get length and adjust for branch
                 double cLength = branch.Length;
-                double lenOne = Rnder(cLength) * lenFactor;
-                double lenTwo = Rnder(cLength) * lenFactor;
+                double lenOne = Rnder(cLength) * GlobVars.lenFactor;
+                double lenTwo = Rnder(cLength) * GlobVars.lenFactor;
 
                 //Generating new end points along vector
-                XYZ newEndOne = end + (end - start).Normalize().Multiply(lenOne); // * lenOne;
+                XYZ newEndOne = end + (end - start).Normalize().Multiply(lenOne); 
                 XYZ newEndTwo = end + (end - start).Normalize().Multiply(lenTwo);
-
-                //XYZ newEndTwo = start + (end - start) * lenTwo;
 
                 //Rotating points to two sides
                 newEndOne = new XYZ(
@@ -117,7 +180,7 @@ namespace FractalTreeGenerator
                     Math.Sin(radPlus) * (newEndOne.X - end.X) + Math.Cos(radPlus) * (newEndOne.Y - end.Y) + end.Y,
                     newEndOne.Z);
                 Line lineOne = Line.CreateBound(end, newEndOne);
-                tree.Add(lineOne);
+                GlobVars.tree.Add(lineOne);
 
                 //call recursive function
                 TreeRecursion(depth - 1, lineOne);
@@ -127,7 +190,7 @@ namespace FractalTreeGenerator
                     Math.Sin(radMinus) * (newEndTwo.X - end.X) + Math.Cos(radMinus) * (newEndTwo.Y - end.Y) + end.Y,
                     newEndTwo.Z);
                 Line lineTwo = Line.CreateBound(end, newEndTwo);
-                tree.Add(lineTwo);
+                GlobVars.tree.Add(lineTwo);
 
                 //call recursive function
                 TreeRecursion(depth - 1, lineTwo);
